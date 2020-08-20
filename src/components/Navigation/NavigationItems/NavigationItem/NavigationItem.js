7487a660564dfef0afafc8b293c6d10dac6c5ca3
sothasil <nikolovskialexxandar@gmail.com>
2020-08-20 20:37:39 +0200

    Added Sidedrawer -Added Sidedrawer component for mobile devices -Refactored some navigation components for Sidedrawer to function properly

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 import './NavigationItem.css';
 
 const navigationItem = (props) => (
-        <div className={props.navbar2 ? "navigation-item2" : "navigation-item"}>
+        <div onClick={props.closeSideDrawer} className={props.navbar2 ? "navigation-item2" : "navigation-item"}>
             <NavLink to={props.link}>{props.children}</NavLink>
         </div>
 );
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 import './SideDrawer.css';
-import Navigation
-const sideDrawer = (props) => {
-    return (
-        <nav>
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../Backdrop/Backdrop';
+import Button from '../../UI/Button/Button';
 
-        </nav>
+const sideDrawer = (props) => {
+    let styleClasses = ['side-drawer', 'closed'];
+    if (props.show) {
+        styleClasses = ['side-drawer', 'open'];
+    }
+
+    return (
+        <React.Fragment>
+            <Backdrop show={props.show} clicked={props.closeSideDrawer} />
+            <nav className={styleClasses.join(' ')}>
+                <NavigationItems closeSideDrawer={props.closeSideDrawer}/>
+                <Button 
+                    styleClasses="small" 
+                    name="X" 
+                    onClick={props.closeSideDrawer}
+                />
+            </nav>
+        </React.Fragment>
     );
 }
 
