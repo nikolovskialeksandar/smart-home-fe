8d9a0733b6e40b11ee34dd2a9b247df9c8d33990
sothasil <nikolovskialexxandar@gmail.com>
2020-09-17 12:10:27 +0200

    Changed authentication form
    - Auth input field validation is now triggered onBlur
    - Removed input validation icons and added error messages
    - Minor auth form style adaptations

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,13 +5,8 @@ import './Input.css';
 const input = (props) => {
     let inputStyles = ['inputElement'];
     let iconValid = null;
-    if (!props.valid && props.touched) {
+    if (!props.validation.valid && props.touched) {
         inputStyles.push('invalid');
-        iconValid= <i className="fa fa-times" aria-hidden="true" style={{color:' #FF5722'}}></i>;
-    }
-
-    else if (props.touched && props.valid) {
-        iconValid= <i className="fa fa-check" aria-hidden="true" style={{color:' #8BC34A'}}></i>;
     }
 
     return (
@@ -19,11 +14,12 @@ const input = (props) => {
             <label className="label">{props.label}</label>
             <input 
                 className={inputStyles.join(' ')}
-                onChange={props.onChange} 
+				onChange={props.onChange}
+				onBlur={props.onBlur}
                 value= {props.value}
                 {...props.elementConfig} 
             />
-            {iconValid}
+			<p className='error-message'>{props.validation.errorMessage}</p>
         </div>
     );
 }
